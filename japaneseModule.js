ef25e1a069ad8ff4981724a53fdcd136ef30e312
Joshua Meyers <joshua.r.meyers@gmail.com>
2017-06-15 10:03:43 -0500

    create jp lexicon

---
+++ japaneseModule.js
@@ -0,0 +1,15 @@
+var translator = (function(jptrans){
+
+	let japaneseLexicon = new Map();
+	console.log("japaneseLexicon", );
+	japaneseLexicon.set("merry", "メッリー");
+	japaneseLexicon.set("christmas","クリストマス");
+	japaneseLexicon.set("and", "と");
+	japaneseLexicon.set("happy new year", "明けましておめでとう");
+
+	console.log("japaneseLexicon", japaneseLexicon);
+	// console.log("0th element", japaneseLexicon.get("merry"));
+	
+	return jptrans
+}(translator));
+
